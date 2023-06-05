712fbb1c6fb1c74588107043efc1a3e562a32c08
c1ach0 <c1ach83500@gmail.com>
2023-06-05 14:50:10 +0200

    docs

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,14 +1,1 @@
-export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"TailVue - TailwindCss & Vue3\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Installation\",\"slug\":\"installation\",\"link\":\"#installation\",\"children\":[]},{\"level\":2,\"title\":\"Body\",\"slug\":\"body\",\"link\":\"#body\",\"children\":[]},{\"level\":2,\"title\":\"Navbar\",\"slug\":\"navbar\",\"link\":\"#navbar\",\"children\":[]},{\"level\":2,\"title\":\"hero banner\",\"slug\":\"hero-banner\",\"link\":\"#hero-banner\",\"children\":[]},{\"level\":2,\"title\":\"Contents\",\"slug\":\"contents\",\"link\":\"#contents\",\"children\":[]},{\"level\":2,\"title\":\"Others\",\"slug\":\"others\",\"link\":\"#others\",\"children\":[]}],\"git\":{\"updatedTime\":1685965570000,\"contributors\":[{\"name\":\"c1ach0\",\"email\":\"c1ach83500@gmail.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
+export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"TailVue - TailwindCss & Vue3\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Installation\",\"slug\":\"installation\",\"link\":\"#installation\",\"children\":[]},{\"level\":2,\"title\":\"Body\",\"slug\":\"body\",\"link\":\"#body\",\"children\":[]},{\"level\":2,\"title\":\"Navbar\",\"slug\":\"navbar\",\"link\":\"#navbar\",\"children\":[]},{\"level\":2,\"title\":\"hero banner\",\"slug\":\"hero-banner\",\"link\":\"#hero-banner\",\"children\":[]},{\"level\":2,\"title\":\"Contents\",\"slug\":\"contents\",\"link\":\"#contents\",\"children\":[]},{\"level\":2,\"title\":\"Others\",\"slug\":\"others\",\"link\":\"#others\",\"children\":[]}],\"git\":{\"updatedTime\":1685968847000,\"contributors\":[{\"name\":\"c1ach0\",\"email\":\"c1ach83500@gmail.com\",\"commits\":2}]},\"filePathRelative\":\"README.md\"}")
